2556030cddb92a726a84bfe75973fd0e75e9e601
2sayle <132142945+2sayle@users.noreply.github.com>
2024-06-29 17:26:02 +0200

    Created snake.py

---
+++ snake.py
@@ -0,0 +1,70 @@
+import curses
+import random
+import time
+
+def main(stdscr):
+    # Configuration initiale
+    curses.curs_set(0)
+    sh, sw = stdscr.getmaxyx()
+    w = curses.newwin(sh, sw, 0, 0)
+    w.keypad(1)
+    w.timeout(100)
+
+    # Position initiale du serpent
+    snk_x = sw // 4
+    snk_y = sh // 2
+    snake = [
+        [snk_y, snk_x],
+        [snk_y, snk_x - 1],
+        [snk_y, snk_x - 2]
+    ]
+
+    # Position initiale de la nourriture
+    food = [sh // 2, sw // 2]
+    w.addch(food[0], food[1], curses.ACS_PI)
+
+    # Direction initiale
+    key = curses.KEY_RIGHT
+
+    while True:
+        next_key = w.getch()
+        key = key if next_key == -1 else next_key
+
+        # Vérifier si le joueur a perdu
+        if (snake[0][0] in [0, sh] or
+            snake[0][1] in [0, sw] or
+            snake[0] in snake[1:]):
+            curses.endwin()
+            quit()
+
+        # Déterminer la nouvelle tête du serpent
+        new_head = [snake[0][0], snake[0][1]]
+
+        if key == curses.KEY_DOWN:
+            new_head[0] += 1
+        if key == curses.KEY_UP:
+            new_head[0] -= 1
+        if key == curses.KEY_LEFT:
+            new_head[1] -= 1
+        if key == curses.KEY_RIGHT:
+            new_head[1] += 1
+
+        snake.insert(0, new_head)
+
+        # Vérifier si le serpent a mangé la nourriture
+        if snake[0] == food:
+            food = None
+            while food is None:
+                nf = [
+                    random.randint(1, sh-1),
+                    random.randint(1, sw-1)
+                ]
+                food = nf if nf not in snake else None
+            w.addch(food[0], food[1], curses.ACS_PI)
+        else:
+            tail = snake.pop()
+            w.addch(tail[0], tail[1], ' ')
+
+        w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
+
+curses.wrapper(main)
