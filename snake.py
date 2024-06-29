7bc70d6a1b0e8803d996c0ad211758e452ef0493
2sayle <132142945+2sayle@users.noreply.github.com>
2024-06-29 18:02:45 +0200

    Fixed some issues

--- snake.py
+++ snake.py
@@ -1,16 +1,18 @@
 import curses
 import random
-import time
+import locale
+
+locale.setlocale(locale.LC_ALL, '')
 
 def main(stdscr):
-    # Configuration initiale
+    # Initial setup
     curses.curs_set(0)
     sh, sw = stdscr.getmaxyx()
     w = curses.newwin(sh, sw, 0, 0)
     w.keypad(1)
     w.timeout(100)
 
-    # Position initiale du serpent
+    # Initial snake position
     snk_x = sw // 4
     snk_y = sh // 2
     snake = [
@@ -19,25 +21,32 @@ def main(stdscr):
         [snk_y, snk_x - 2]
     ]
 
-    # Position initiale de la nourriture
+    # Initial food position
     food = [sh // 2, sw // 2]
-    w.addch(food[0], food[1], curses.ACS_PI)
+    w.addch(food[0], food[1], '@')
 
-    # Direction initiale
+    # Initial direction
     key = curses.KEY_RIGHT
 
     while True:
         next_key = w.getch()
-        key = key if next_key == -1 else next_key
+        
+        # Prevent reversing direction
+        if next_key != -1:
+            if (key == curses.KEY_DOWN and next_key != curses.KEY_UP) or \
+               (key == curses.KEY_UP and next_key != curses.KEY_DOWN) or \
+               (key == curses.KEY_LEFT and next_key != curses.KEY_RIGHT) or \
+               (key == curses.KEY_RIGHT and next_key != curses.KEY_LEFT):
+                key = next_key
 
-        # Vérifier si le joueur a perdu
-        if (snake[0][0] in [0, sh] or
-            snake[0][1] in [0, sw] or
+        # Check if the player has lost
+        if (snake[0][0] in [0, sh-1] or
+            snake[0][1] in [0, sw-1] or
             snake[0] in snake[1:]):
             curses.endwin()
             quit()
 
-        # Déterminer la nouvelle tête du serpent
+        # Determine the new head of the snake
         new_head = [snake[0][0], snake[0][1]]
 
         if key == curses.KEY_DOWN:
@@ -51,20 +60,30 @@ def main(stdscr):
 
         snake.insert(0, new_head)
 
-        # Vérifier si le serpent a mangé la nourriture
+        # Check if the snake has eaten the food
         if snake[0] == food:
             food = None
             while food is None:
                 nf = [
-                    random.randint(1, sh-1),
-                    random.randint(1, sw-1)
+                    random.randint(1, sh-2),
+                    random.randint(1, sw-2)
                 ]
                 food = nf if nf not in snake else None
-            w.addch(food[0], food[1], curses.ACS_PI)
+            w.addch(food[0], food[1], '@')
         else:
             tail = snake.pop()
             w.addch(tail[0], tail[1], ' ')
 
-        w.addch(snake[0][0], snake[0][1], curses.ACS_CKBOARD)
+        # Redraw the snake
+        for i, segment in enumerate(snake):
+            if i == 0:
+                w.addch(segment[0], segment[1], 'O')
+            else:
+                w.addch(segment[0], segment[1], 'o')
+
+        # Redraw the food to ensure it's always visible
+        w.addch(food[0], food[1], '@')
+
+        w.refresh()
 
 curses.wrapper(main)
